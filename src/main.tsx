6e6e13f286e627bce503a9ac07a4197ee4df2e4a
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-10-30 16:15:10 +0800

    feat: fetch data from another repo

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,30 @@ import ReactDOM from 'react-dom';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/lib/locale/zh_CN';
 import 'antd/dist/antd.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { HashRouter } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
 import routes from 'virtual:generated-pages-react';
 import Layout from '@/layout';
 import 'virtual:windi.css';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <ConfigProvider locale={zhCN}>
-      <HashRouter basename={import.meta.env.BASE_URL}>
-        <Layout>{renderRoutes(routes)}</Layout>
-      </HashRouter>
-    </ConfigProvider>
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider locale={zhCN}>
+        <HashRouter basename={import.meta.env.BASE_URL}>
+          <Layout>{renderRoutes(routes)}</Layout>
+        </HashRouter>
+      </ConfigProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.querySelector('#root'),
 );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,110 @@
-import { memo, useState, useMemo } from 'react';
-import { Row, Col, Input, Typography, Divider, List, Card } from 'antd';
-import data from '@/data';
+import { memo, useState, useMemo, useCallback } from 'react';
+import { Row, Col, Input, Typography, Divider, List, Card, Spin, Result } from 'antd';
+import { useQuery, useQueries } from 'react-query';
+import { useLocation } from 'react-use';
 
 const { Link } = Typography;
 
+type Alphabet =
+  | 'A'
+  | 'B'
+  | 'C'
+  | 'D'
+  | 'E'
+  | 'F'
+  | 'G'
+  | 'H'
+  | 'I'
+  | 'J'
+  | 'K'
+  | 'L'
+  | 'M'
+  | 'N'
+  | 'O'
+  | 'P'
+  | 'Q'
+  | 'R'
+  | 'S'
+  | 'T'
+  | 'U'
+  | 'V'
+  | 'W'
+  | 'X'
+  | 'Y'
+  | 'Z';
+
+const alphabet = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+
+type JSONData = [Alphabet, { [propName: string]: string[] }][];
+
 const Index = memo(() => {
   const [searchText, setSearchText] = useState('');
-  const filteredData = useMemo(
+
+  const location = useLocation();
+  const url = useCallback(
+    (name: string) => {
+      const { href } = location;
+      if (href?.includes('gitee')) {
+        return `https://gitee.com/millcloud/glossary-json/raw/main/${name}.json`;
+      }
+      return `https://raw.githubusercontent.com/MillCloud/glossary-json/main/${name}.json`;
+    },
+    [location],
+  );
+  const headers = useMemo(() => {
+    const { href } = location;
+    if (href?.includes('gitee')) {
+      return;
+    }
+    // eslint-disable-next-line consistent-return
+    return { Accept: 'application/vnd.github.v3+json' };
+  }, [location]);
+  const results = useQueries(
+    alphabet.map((item) => ({
+      queryKey: ['json', item],
+      queryFn: () => fetch(url(item), { headers }).then((response) => response.json()),
+    })),
+  );
+  const isLoading = useMemo(() => results.some((item) => item.isLoading), [results]);
+  const error = useMemo(() => results.find((item) => item.error)?.error, [results]);
+  const data = useMemo(
     () =>
-      data.filter(
-        ([, object]) =>
+      (results.map((item, index) => [alphabet[index], item.data]) as JSONData).filter(
+        ([, object = {}]) =>
           Object.keys(object).length > 0 &&
           Object.entries(object).some(
             ([k, v]) => k.includes(searchText) || v.some((item) => item.includes(searchText)),
           ),
       ),
-    [searchText],
+    [results, searchText],
   );
-
-  console.log('filteredData', filteredData);
 
   return (
     <>
@@ -41,32 +127,44 @@ const Index = memo(() => {
           试试别的
         </Link>
       </Row>
-      {filteredData.map(([alphabet, object]) => (
-        <>
-          <Divider orientation="left">{alphabet}</Divider>
-          <List
-            grid={{
-              gutter: 16,
-              xs: 1,
-              sm: 2,
-              md: 3,
-              lg: 4,
-              xl: 4,
-              xxl: 4,
-            }}
-            dataSource={Object.entries(object)}
-            renderItem={(item) => (
-              <List.Item>
-                <Card title={item[0]}>
-                  {item[1].map((string) => (
-                    <Typography>{string}</Typography>
-                  ))}
-                </Card>
-              </List.Item>
-            )}
-          />
-        </>
-      ))}
+      {isLoading && (
+        <Row justify="center" className="my-4">
+          <Spin />
+        </Row>
+      )}
+      {!isLoading && error && (
+        // @ts-ignore
+        <Result status="error" title={(error?.message ?? error?.toString() ?? error) as string} />
+      )}
+      {!isLoading &&
+        !error &&
+        data.map(([char, object]) => (
+          <>
+            <Divider orientation="left">{char}</Divider>
+            <List
+              grid={{
+                gutter: 16,
+                xs: 1,
+                sm: 2,
+                md: 3,
+                lg: 4,
+                xl: 4,
+                xxl: 4,
+              }}
+              dataSource={Object.entries(object)}
+              rowKey={(item) => item[0]}
+              renderItem={(item) => (
+                <List.Item key={item[0]}>
+                  <Card title={item[0]}>
+                    {item[1].map((string) => (
+                      <Typography>{string}</Typography>
+                    ))}
+                  </Card>
+                </List.Item>
+              )}
+            />
+          </>
+        ))}
     </>
   );
 });
