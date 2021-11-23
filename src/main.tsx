f283a19445e0830a50384bf450be76dd6e7f879c
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-11-23 15:39:34 +0800

    feat: antd variables

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/lib/locale/zh_CN';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.variable.min.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { HashRouter } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
